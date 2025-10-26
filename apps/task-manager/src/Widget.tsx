74b5983e6baaf6ae274d33dca3fd91c46aef0e34
Kartik Chindarkar <karthikchindarkar@gmail.com>
2025-10-25 19:22:33 -0700

    fix

--- apps/task-manager/src/Widget.tsx
+++ apps/task-manager/src/Widget.tsx
@@ -1,6 +1,7 @@
 // apps/task-manager/src/Widget.tsx
 import React from "react";
 import TaskManager from "./pages/task-manager";
+import "./index.css";  // Import styles at the Widget level
 
 export default function Widget() {
   return <TaskManager />;
