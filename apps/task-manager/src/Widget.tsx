11c5fa914440310354f5b6bfa809729b39245eb8
Kartik Chindarkar <karthikchindarkar@gmail.com>
2025-10-26 23:23:57 -0700

    try fix2

--- apps/task-manager/src/Widget.tsx
+++ apps/task-manager/src/Widget.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import {TaskManager} from "./pages/task-manager";
 import "@efficio/ui";
-import "@shared-design-token/global.css";
 
 export default function Widget() {
   return <TaskManager />;
