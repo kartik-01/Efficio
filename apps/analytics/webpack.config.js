efdfa07ac54a82c21711182ab2b46a73e83f0f43
Kartik Chindarkar <karthikchindarkar@gmail.com>
2025-10-30 22:43:28 -0700

    deployment config

--- apps/analytics/webpack.config.js
+++ apps/analytics/webpack.config.js
@@ -1,14 +1,23 @@
 const path = require("path");
 const createWebpackConfig = require("../../configs/createWebpackConfig");
 
-module.exports = createWebpackConfig({
-  appName: "analytics",
-  port: process.env.PORT || 3003,
-  moduleFederation: {
-    name: "analytics",
-    exposes: {
-      "./Module": path.resolve(__dirname, "src/Module")
-    }
-  }
-});
+const isProduction = process.env.NODE_ENV === "production";
 
+module.exports = createWebpackConfig({
+  appName: "dashboard-app-shell",
+  port: process.env.PORT || 3000,
+  moduleFederation: {
+    name: "app_shell",
+    remotes: isProduction
+      ? {
+          task_manager: "task_manager@https://test-effici0-tasks.netlify.app/remoteEntry.js",
+          time_tracker: "time_tracker@https://test-effici0-time.netlify.app/remoteEntry.js",
+          analytics: "analytics@https://test-effici0-analytics.netlify.app/remoteEntry.js"
+        }
+      : {
+          task_manager: "task_manager@http://localhost:3001/remoteEntry.js",
+          time_tracker: "time_tracker@http://localhost:3002/remoteEntry.js",
+          analytics: "analytics@http://localhost:3003/remoteEntry.js"
+        }
+  }
+}); 
